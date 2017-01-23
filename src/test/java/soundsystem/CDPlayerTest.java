9edacc2e3573f1891494abb8f0da79b76ebbe891
Michael Smith <ketrimf@gmail.com>
2017-01-23 17:50:33 +0100

    Improved wiring in test, added JSR-330 annotations

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -8,15 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.inject.Inject;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
 
     @Autowired
+    private MediaPlayer player;
+
+    @Inject
     private CompactDisc cd;
 
     @Test
     public void cdShouldNotBeNull() {
         Assert.assertNotNull(cd);
+    }
+
+    @Test
+    public void play() {
+        Assert.assertNotNull(player);
+        player.play();
     }
 }
